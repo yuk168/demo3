6f0ca7d40229a41d8e928bd59db504ec7ff72d9d
yuk168 <282589485@qq.com>
2019-06-13 11:27:25 +0800

    测试git,新增一个类和里面的2个方法。

---
+++ src/com/test0/TestGit.java
@@ -0,0 +1,23 @@
+package com.test0;
+
+import org.junit.Test;
+
+/**
+ * 仅仅是为了测试git的版本控制。多人开发的时候。
+ * 更新，提交，同步的操作。
+ * @author Administrator
+ *
+ */
+public class TestGit {
+
+	@Test
+	public void test1() {
+		System.out.println("1111111111");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("22222222222");
+	}
+	
+}
